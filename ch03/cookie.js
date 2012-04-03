6e093aa7db2d2cbfd6f7c39bd7bb9035f7d9617b
KJlmfe <kjlmfe@gmail.com>
2012-04-03 10:58:53 +0800

    完成第三章内容(cookie 写 读 清除)

---
+++ ch03/cookie.js
@@ -0,0 +1,33 @@
+function writeCookie(name,value,days)
+{
+	var expires = "";
+		
+	if(days)
+	{
+		var date = new Date();
+		date.setTime(date.getTime() + (days*24*60*60*1000));
+		expires = ";expires=" + date.toGMTString();
+	}
+	var cookie = name + "=" + value + expires + "; path=/";
+	alert(cookie);
+	document.cookie = cookie;
+}
+
+function readCookie(name)
+{
+	var searchName = name + "=";
+	var cookies = document.cookie.split(';');
+	for(var i=0;i<cookies.length; i++)
+	{
+		var c = cookies[i];
+		while(c.charAt[0] == ' ')
+			c = c.substring(1,c.length);
+		if(c.indexOf(searchName) == 0)
+			return c.substring(searchName.length,c.length);
+	}
+	return null;
+}
+function eraseCookie(name)
+{
+	writeCookie(name,"",-1);
+}
